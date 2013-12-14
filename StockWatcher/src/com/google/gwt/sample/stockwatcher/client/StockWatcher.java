ec63f2a72dd1b765bb1e79bf2a56f0bec63d388c
ashok2ashok@gmail.com <ashok2ashok@gmail.com@f1c89ab3-9d3d-87b4-bcd3-2919039d2599>
2013-12-14 02:35:12 +0000

    Incomplete - task : StockWatcher Practice

---
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -0,0 +1,46 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class StockWatcher implements EntryPoint {
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
+	private Button addStockButton = new Button("Add");
+	private Label lastUpdatedLabel = new Label();
+	@Override
+	public void onModuleLoad() {
+		// TODO Create table for stock data.  
+		stocksFlexTable.setText(0, 0, "Symbol");
+		stocksFlexTable.setText(0, 1, "Price");
+		stocksFlexTable.setText(0, 2, "Change");
+		stocksFlexTable.setText(0, 3, "Remove");
+		
+		// TODO Assemble Add Stock panel.  
+		addPanel.add(newSymbolTextBox);
+		addPanel.add(addStockButton);
+		
+		// TODO Assemble Main panel.
+		mainPanel.add(stocksFlexTable);
+		mainPanel.add(addPanel);
+		mainPanel.add(lastUpdatedLabel);
+		
+		// TODO Associate the Main panel with the HTML host page.
+		RootPanel.get("stockList").add(mainPanel);
+		
+		// TODO Move cursor focus to the input box.
+		newSymbolTextBox.setFocus(true);
+
+	}
+}
